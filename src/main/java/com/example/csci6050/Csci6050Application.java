e2c99778abfd3c3033c256b6585c4785831691dd
Sean <sk84758@uga.edu>
2022-03-19 14:53:42 -0400

    Initial Commit: Basic Structure for Spring Boot

---
+++ src/main/java/com/example/csci6050/Csci6050Application.java
@@ -0,0 +1,13 @@
+package com.example.csci6050;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csci6050Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csci6050Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csci6050/Csci6050ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csci6050;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csci6050ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
