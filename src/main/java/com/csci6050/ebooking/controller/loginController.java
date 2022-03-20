f422ba2810c0bc22bcac37aec0dcf19376db9545
Sean <sk84758@uga.edu>
2022-03-20 13:40:34 -0400

    Third Commit: controller and html modified

--- src/main/java/com/csci6050/ebooking/controller/loginController.java
+++ src/main/java/com/csci6050/ebooking/controller/loginController.java
@@ -3,14 +3,21 @@ package com.csci6050.ebooking.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class loginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String registerPage(){
+
         return "login";
     }
 
-
+    @ResponseBody
+    @RequestMapping("/login2")
+    public String test(String message) {
+        System.out.println(message);
+        return "yes";
+    }
 }
