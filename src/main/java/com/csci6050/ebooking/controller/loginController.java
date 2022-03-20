82da416f96a4996a7fa5458b4644de9f25f32b4c
Sean <sk84758@uga.edu>
2022-03-20 14:25:50 -0400

    Forth Commit: controller and html modified

--- src/main/java/com/csci6050/ebooking/controller/loginController.java
+++ src/main/java/com/csci6050/ebooking/controller/loginController.java
@@ -1,9 +1,15 @@
 package com.csci6050.ebooking.controller;
 
+import com.csci6050.ebooking.bean.users;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class loginController {
@@ -16,8 +22,11 @@ public class loginController {
 
     @ResponseBody
     @RequestMapping("/login2")
-    public String test(String message) {
-        System.out.println(message);
+    public String test(users message) throws ServletException, IOException {
+        String username = message.getEmail();
+        String password = message.getPassword();
+        System.out.println(username);
+        System.out.println(password);
         return "yes";
     }
 }
