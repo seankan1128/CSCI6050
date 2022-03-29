85ee6747e7dd51813e7b807b5420d2c09868c4e5
Sean <sk84758@uga.edu>
2022-03-29 16:24:14 -0400

    loginController changed to return Map

---
+++ src/main/java/com/csci6050/ebooking/DTO/login_UP.java
@@ -0,0 +1,81 @@
+package com.csci6050.ebooking.DTO;
+
+import com.csci6050.ebooking.entity.Paymentcard;
+import com.csci6050.ebooking.entity.User;
+
+import java.util.List;
+
+public class login_UP {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String birthday;
+    private String enrolledForPromotions;
+    private int userType;
+    private List<Paymentcard> PaymentCardList;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEnrolledForPromotions() {
+        return enrolledForPromotions;
+    }
+
+    public void setEnrolledForPromotions(String enrolledForPromotions) {
+        this.enrolledForPromotions = enrolledForPromotions;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+
+    public List<Paymentcard> getPaymentCardList() {
+        return PaymentCardList;
+    }
+
+    public void setPaymentCardList(List<Paymentcard> paymentCardList) {
+        PaymentCardList = paymentCardList;
+    }
+}
--- src/main/java/com/csci6050/ebooking/controller/loginController.java
+++ src/main/java/com/csci6050/ebooking/controller/loginController.java
@@ -1,6 +1,7 @@
 package com.csci6050.ebooking.controller;
 
-import com.csci6050.ebooking.DTO.login_UPSD;
+import com.csci6050.ebooking.DTO.StatusNDescription;
+import com.csci6050.ebooking.DTO.login_UP;
 import com.csci6050.ebooking.encrypt.passwordEncrypt;
 import com.csci6050.ebooking.entity.Paymentcard;
 import com.csci6050.ebooking.entity.User;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,10 +33,10 @@ public class loginController {
 
     @ResponseBody
     @RequestMapping("/login2")
-    public login_UPSD returnJson(User data) throws IOException {
-//        Map<String, Object> returnMap = new HashMap<>(4);
+    public Map<String, Object> returnJson(User data) throws IOException {
+        Map<String, Object> returnMap = new HashMap<>();
         User n = userRepository.findByEmail(data.getEmail());
-        login_UPSD upsd = new login_UPSD();
+        StatusNDescription SD = new StatusNDescription();
         String description = "";
 
         if(n == null){
@@ -42,9 +44,10 @@ public class loginController {
             description = "You do not have an account.";
 //            returnMap.put("Status", 0);
 //            returnMap.put("Description", description);
-            upsd.setStatus(0);
-            upsd.setDescription(description);
-            return upsd;
+            SD.setStatus(0);
+            SD.setDescription(description);
+            returnMap.put("ReturnStatus", SD);
+            return returnMap;
         } else {
             passwordEncrypt en = new passwordEncrypt();
             String encrypt = en.encrypt(data.getPassword());
@@ -54,58 +57,77 @@ public class loginController {
                 description = "Your password is incorrect.";
 //                returnMap.put("Status", 0);
 //                returnMap.put("Description", description);
-                upsd.setStatus(0);
-                upsd.setDescription(description);
-                return upsd;
+                SD.setStatus(0);
+                SD.setDescription(description);
+                returnMap.put("ReturnStatus", SD);
+                return returnMap;
             }
 
-            data.setFirstName(n.getFirstName());
-            data.setLastName(n.getLastName());
-            data.setPassword("");
-            data.setUserType(n.getUserType());
-            data.setEmail(n.getEmail());
-            data.setPhone(n.getPhone());
-            data.setStatus(n.getStatus());
-            data.setEnrolledForPromotions(n.getEnrolledForPromotions());
-//            returnMap.put("User", data);
-            upsd.setUser(data);
+            if(n.getStatus() == 2){
+                System.out.println("Your account is inactive");
+                description = "Your account is inactive";
+                SD.setStatus(0);
+                SD.setDescription(description);
+                returnMap.put("ReturnStatus", SD);
+                return returnMap;
+            }
+            if(n.getStatus() == 3){
+                System.out.println("Your account is suspended");
+                description = "Your account is suspended. Please contact an administrator.";
+                SD.setStatus(0);
+                SD.setDescription(description);
+                returnMap.put("ReturnStatus", SD);
+                return returnMap;
+            }
+
+            login_UP up = new login_UP();
+
+            up.setFirstname(n.getFirstName());
+            up.setLastname(n.getLastName());
+            up.setUserType(n.getUserType());
+            up.setEmail(n.getEmail());
+            up.setPhone(n.getPhone());
+            up.setUserType(n.getUserType());
+            up.setEnrolledForPromotions(n.getEnrolledForPromotions());
 
             Iterable<Paymentcard> pList = paymentcardRepository.findAllByUser(n);
             List<Paymentcard> paymentcardlist = new ArrayList<>();
             pList.forEach(paymentcardlist::add);
-            upsd.setPaymentCardList(paymentcardlist);
+            up.setPaymentCardList(paymentcardlist);
 
-            int status = 0;
+            returnMap.put("ReturnUser", up);
+
+
+            int status;
 
             if(n.getStatus() == 1){
                 if(n.getUserType() == 1) {
                     System.out.println("You are an admin");
                     status = 1;
                     description = "You are an admin";
+                    SD.setStatus(status);
+                    SD.setDescription(description);
                 }
                 else if(n.getUserType() == 2) {
                     System.out.println("You are a customer");
                     status = 1;
                     description = "You are a customer";
+                    SD.setStatus(status);
+                    SD.setDescription(description);
                 }
                 else if(n.getUserType() == 3) {
                     System.out.println("You are an employee");
                     status = 1;
                     description = "You are an employee";
+                    SD.setStatus(status);
+                    SD.setDescription(description);
                 }
             }
-            if(n.getStatus() == 2){
-                System.out.println("Your account is inactive");
-                description = "Your account is inactive";
-            }
-            if(n.getStatus() == 3){
-                System.out.println("Your account is suspended");
-                description = "Your account is suspended. Please contact an administrator.";
-            }
-            upsd.setStatus(status);
-            upsd.setDescription(description);
+
+            returnMap.put("ReturnStatus", SD);
+
 
         }
-        return upsd;
+        return returnMap;
     }
 }
