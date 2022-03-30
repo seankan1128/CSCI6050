18f1100a9cdf67b48ae3f4d8f200a0e1ed286584
Sean <sk84758@uga.edu>
2022-03-29 22:10:22 -0400

    Edit profile first form

--- src/main/java/com/csci6050/ebooking/controller/editController.java
+++ src/main/java/com/csci6050/ebooking/controller/editController.java
@@ -1,10 +1,15 @@
 package com.csci6050.ebooking.controller;
 
+import com.csci6050.ebooking.DTO.StatusNDescription;
+import com.csci6050.ebooking.entity.User;
 import com.csci6050.ebooking.repository.PaymentcardRepository;
 import com.csci6050.ebooking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller // This means that this class is a Controller
 public class editController {
@@ -17,5 +22,20 @@ public class editController {
     @RequestMapping(value = "/editprofile", method = RequestMethod.GET)
     public String editProfilePage(){
         return "edit_profile";
+    }
+
+    @ResponseBody
+    @RequestMapping("userinfo")
+    public Map<String, Object> edituserinfo(User user){
+        Map<String, Object> returnMap = new HashMap<>();
+        StatusNDescription SD = new StatusNDescription();
+        SD.setStatus(1);
+        SD.setDescription("Account information changed");
+        User n = userRepository.findByEmail(user.getEmail());
+        n.setFirstName(user.getFirstName());
+        n.setLastName(user.getLastName());
+        n.setEnrolledForPromotions(user.getEnrolledForPromotions());
+        userRepository.save(n);
+        return returnMap;
     }
 }
