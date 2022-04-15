c17e3be00ebda8941ad897f250da5cfa18137dff
Sean <sk84758@uga.edu>
2022-04-15 18:37:22 -0400

    first draft for add movie

--- src/main/java/com/csci6050/ebooking/controller/addMovieController.java
+++ src/main/java/com/csci6050/ebooking/controller/addMovieController.java
@@ -1,5 +1,6 @@
 package com.csci6050.ebooking.controller;
 
+import com.csci6050.ebooking.DTO.StatusNDescription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import com.csci6050.ebooking.entity.Movie;
 import com.csci6050.ebooking.repository.MovieRepository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class addMovieController {
     @Autowired
@@ -15,11 +19,18 @@ public class addMovieController {
 
     @ResponseBody
     @RequestMapping("addmovieform")
-    public int addNewMovie(Movie movie){
+    public Map<String, Object> addNewMovie(Movie movie){
         Movie mov = movieRepository.findById(movie.getId());
+
+        Map<String, Object> returnMap = new HashMap<>();
+        StatusNDescription SD = new StatusNDescription();
+
         if(mov != null){
             System.out.println("Movie already exists");
-            return 0;
+            SD.setStatus(0);
+            SD.setDescription("Movie already exists");
+            returnMap.put("ReturnStatus",SD);
+            return returnMap;
         }
 
         Movie m = new Movie();
@@ -28,13 +39,16 @@ public class addMovieController {
         m.setGenre(movie.getGenre());
         m.setProducer(movie.getProducer());
         m.setDuration(movie.getDuration());
-        m.setTrailerPicture(movie.getTrailerPicture());
+//        m.setTrailerPicture(movie.getTrailerPicture());
         m.setTrailerVideo(movie.getTrailerVideo());
         m.setReview(movie.getReview());
         m.setRatingID(movie.getRatingID());
 
         movieRepository.save(m);
 
-        return 1;
+        SD.setStatus(1);
+        SD.setDescription("Successfully saved");
+        returnMap.put("ReturnStatus",SD);
+        return returnMap;
     }
 }
