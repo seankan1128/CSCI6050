ba85c947698d3fc2bbc36c3144249b2d2e545d0e
Sean <sk84758@uga.edu>
2022-04-15 19:19:42 -0400

    first draft for add movie (added category)

--- src/main/java/com/csci6050/ebooking/controller/addMovieController.java
+++ src/main/java/com/csci6050/ebooking/controller/addMovieController.java
@@ -1,6 +1,9 @@
 package com.csci6050.ebooking.controller;
 
 import com.csci6050.ebooking.DTO.StatusNDescription;
+import com.csci6050.ebooking.entity.Paymentcard;
+import com.csci6050.ebooking.entity.Promotions;
+import com.csci6050.ebooking.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
@@ -9,7 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import com.csci6050.ebooking.entity.Movie;
 import com.csci6050.ebooking.repository.MovieRepository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -38,7 +43,7 @@ public class addMovieController {
         m.setCast(movie.getCast());
         m.setCategory(movie.getCategory());
         m.setGenre(movie.getGenre());
-        m.setProducer(movie.getProducer());
+        m.setDirector(movie.getDirector());
         m.setDuration(movie.getDuration());
 //        m.setTrailerPicture(movie.getTrailerPicture());
         m.setTrailerVideo(movie.getTrailerVideo());
@@ -46,10 +51,13 @@ public class addMovieController {
         m.setRatingID(movie.getRatingID());
 
         movieRepository.save(m);
-
+        Iterable<Movie> mlist = movieRepository.findAll();
+        List<Movie> movieList = new ArrayList<>();
+        mlist.forEach(movieList::add);
         SD.setStatus(1);
         SD.setDescription("Successfully saved");
         returnMap.put("ReturnStatus",SD);
+        returnMap.put("MovieList",movieList);
         return returnMap;
     }
 }
