a6cc3bb00ccd551a6a508bb6ff0af525fbc1f122
Sean <sk84758@uga.edu>
2022-04-17 16:00:35 -0400

    intermediate push (have error in main controller)

--- src/main/java/com/csci6050/ebooking/controller/mainController.java
+++ src/main/java/com/csci6050/ebooking/controller/mainController.java
@@ -2,11 +2,17 @@ package com.csci6050.ebooking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // This means that this class is a Controller
 public class mainController {
     @GetMapping("/")
-    public String forgetPassword(){
+    public String mainpage(){
         return "index";
     }
+
+    @ResponseBody
+    @RequestMapping("")
+
 }
