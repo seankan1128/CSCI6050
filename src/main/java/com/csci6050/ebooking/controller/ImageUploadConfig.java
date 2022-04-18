5ab17594cf707fb7b39e48adbc8256aef58c3cda
Sean <sk84758@uga.edu>
2022-04-17 22:23:13 -0400

    done photo problem

--- src/main/java/com/csci6050/ebooking/controller/ImageUploadConfig.java
+++ src/main/java/com/csci6050/ebooking/controller/ImageUploadConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ImageUploadConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("file:C:\\Users\\Sean\\IdeaProjects\\ebooking\\src\\main\\resources\\static\\image\\");
+        registry.addResourceHandler("/image/**").addResourceLocations("file:src\\main\\resources\\static\\image\\");
     }
 }
