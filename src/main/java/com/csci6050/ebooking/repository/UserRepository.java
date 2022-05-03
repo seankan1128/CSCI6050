bfdd504f735f6d5d4b66a055b491efc38b470a34
Dartz <ethan.w.mok@gmail.com>
2022-05-03 17:08:52 -0400

    Update

--- src/main/java/com/csci6050/ebooking/repository/UserRepository.java
+++ src/main/java/com/csci6050/ebooking/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.csci6050.ebooking.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
 import com.csci6050.ebooking.entity.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
