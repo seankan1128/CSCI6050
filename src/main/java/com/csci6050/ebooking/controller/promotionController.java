a496a69c09d62d6c2d431d3f926fb0b3b50ddab0
Sean <sk84758@uga.edu>
2022-05-05 13:05:07 -0400

    add userController

--- src/main/java/com/csci6050/ebooking/controller/promotionController.java
+++ src/main/java/com/csci6050/ebooking/controller/promotionController.java
@@ -1,7 +1,6 @@
 package com.csci6050.ebooking.controller;
 
 import com.csci6050.ebooking.DTO.StatusNDescription;
-import com.csci6050.ebooking.entity.Paymentcard;
 import com.csci6050.ebooking.entity.Promotions;
 import com.csci6050.ebooking.entity.User;
 import com.csci6050.ebooking.repository.PromotionsRepository;
@@ -9,15 +8,9 @@ import com.csci6050.ebooking.repository.UserRepository;
 import com.csci6050.ebooking.tool.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
